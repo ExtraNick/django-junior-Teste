4e49b51b8469be13ef50ddad74dc2a587d1a52fa
Nick <NicolasNeryTeles@hotmail.com>
2025-02-24 01:48:05 -0300

    Arquivos desafio finalizado
    Codigo final do desafio, represendando tutorial django parte 4 + requerimentos do desafio

--- polls/models.py
+++ polls/models.py
@@ -2,27 +2,29 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
+from django.db.models import Sum
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    #Metodos
     def __str__(self):
         return self.question_text
-
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-    def total_votes(self):
-        votes = 0
-        choices = self.choice_set.all()
-        # Faça um laço para somar todos os votos.
-        return votes
-
-    def has_votes(self):
-        # Utilize uma condição para retornar se essa Questão tem ou não votos.
-        return False
+    
+    #Metodos desafio
+    #usage: Question.total_votes(Question)
+    def total_votes(self): 
+        return Choice.objects.aggregate(Sum('votes'))
+    #usage: Question.has_votes(Question)
+    def has_votes(self): 
+        check_status = Question.objects.filter(choice__votes__isnull=False)
+        if str(check_status) == "<QuerySet []>": #TO-DO: realizar essa comparacao sem o matching de string. I.E: If Query Null 
+            return False
+        else: 
+            return True
 
 
 class Choice(models.Model):
--- polls/urls.py
+++ polls/urls.py
@@ -9,3 +9,18 @@ urlpatterns = [
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+#Comentario codigo pre-tutorial django 4
+# from . import views
+
+# app_name = "polls"
+# urlpatterns = [
+#     # ex:/polls/ <- default page
+#     path("", views.index, name="index"),
+#     # ex: /polls/5/ <- page of a poll's id
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     #ex: /polls/5/results/ <- page of results of a poll's id
+#     path("<int:question_id>/results/", views.results, name="results"),
+#     #ex: /polls/5/vote/ <- display votes of a poll's id
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+# ]
